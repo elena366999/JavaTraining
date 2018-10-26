154d18c4564871de7d75378b609b43e7876a0b56
Alena Semianitskaya <Alena_Semianitskaya@epam.com>
2018-10-26 16:27:44 +0300

    fix countArrayElementSum() method

--- src/main/java/by/epam/training/task1/ArrayElementSumCounter.java
+++ src/main/java/by/epam/training/task1/ArrayElementSumCounter.java
@@ -4,11 +4,15 @@ public class ArrayElementSumCounter {
 
     public static int countArrayElementSum(int[] array, int start, int finish) {
 
-        if (start < 0 || finish < 0 || start > finish) {
+        if (start < 0 || finish < 0 || start > finish ) {
             return 0;
         }
 
         if (array == null || array.length == 0) {
+            return 0;
+        }
+
+        if(start>array.length || finish>array.length){
             return 0;
         }
 
